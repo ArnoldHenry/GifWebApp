912ba2ab3a5f44f68200f7c966841e8499248d30
ArnoldHenry <arnoldomwoyo2@gmail.com>
2018-09-21 11:27:56 +0300

    added files and folders for GifWebbApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import {Http,Response} from '@angular/http';
+import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'GiphySearch';
+  title = 'Giphy WebApp';
+  trendingURL = "http://api.giphy.com/v1/gifs/trending";
+  linkSearch = "http://api.giphy.com/v1/gifs/search?q=";
+  http:Http;
+  giphies=[];
+  trendgifs=[];
+  page: number=1;
+  
+  constructor(http:Http){
+    this.http = http;
+
+  //  var giflink = this.link;
+  //  console.log(giflink);
+
+   var fullLInk = this.trendingURL+"?api_key="+environment.API;
+   
+    this.http.request(fullLInk)
+    .subscribe((newres:Response)=>{
+      this.trendgifs = newres.json().data
+      console.log(newres.json().data);
+    })
+  } 
+  onScroll(){
+    console.log("scrolled");  
+    this.page = this.page + 1;  
+    this.constructor(); 
+  }
+
+
+  PerformSearch(SearchTerm: HTMLInputElement):void{
+    
+    var apiLink = this.linkSearch + SearchTerm.value+"&api_key="+environment.API;
+    console.log(apiLink);
+    
+    this.http.request(apiLink)
+        .subscribe((res:Response)=>{
+          this.giphies = res.json().data
+          console.log(res.json());
+        });
+  }  
+
+
 }
